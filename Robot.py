fc5054704d6e3603f456fcd101b52e8bd52d1597
Stavros Kaskuras <stavroskaskuras@gmail.com>
2023-12-17 21:33:53 +0200

    Update Robot.py

--- Robot.py
+++ Robot.py
@@ -8,16 +8,14 @@ if __name__ == '__main__':
     pwm.setPWMFreq(50)
     try:
         while True:
+            channel_input = input("Enter channel number (0-15): ")
             on_input = input("Enter 'on' value (0-4095): ")
             off_input = input("Enter 'off' value (0-4095): ")
 
+            channel = int(channel_input)
             on_value = int(on_input)
             off_value = int(off_input)
 
-            if 0 <= on_value <= 4095 and 0 <= off_value <= 4095:
-                pwm.setPWM(0, on_value, off_value)
-                print(f"Set PWM values: 'on' = {on_value}, 'off' = {off_value}")
-            else:
-                print("Invalid input. Values should be between 0 and 4095.")
-    except KeyboardInterrupt:
-        print("\nExiting the program.")
+            if 0 <= channel <= 15 and 0 <= on_value <= 4095 and 0 <= off_value <= 4095:
+                pwm.setPWM(channel, on_value, off_value)
+                print(f"Set PWM values for channel {channel}: 'on' = {on_value}, 'off' = {off_value}
